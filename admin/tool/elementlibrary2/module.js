c7406cb3cbe3df2e6e5d8e424e731205da0a56f0
Nils Sanderson <nils@learningpool.com>
2013-07-16 09:53:51 +0100

    Uber Theme - Added Element Library 2 which houses extra testing elements. (from git@github.com:samhemelryk/moodle.git)

---
+++ admin/tool/elementlibrary2/module.js
@@ -0,0 +1,8 @@
+
+M.tool_elementlibrary2 = M.tool_elementlibrary2 || {};
+
+M.tool_elementlibrary2.prettyprint = function(Y) {
+    Y.on("domready", function (e) {
+        prettyPrint();
+    });
+};
